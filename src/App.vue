eb2bd072dc085023a9f2f4ba2f554b83fe54674e
inoue-yu <iyuta1124@gmail.com>
2022-08-15 22:48:37 +0900

    不溶な記述を削除

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import { ref, computed } from 'vue'
+
+const Constants = {
+  inc: '',
+  usedate: '日付',
+  name: '内容',
+  usedAmount: '金額(円)',
+  financial: '保有金融機関',
+  largeCategory: '大項目',
+  smallCategory: '中項目',
+}
+
+// data
 const moneyForwardCsv = ref([])
 const message = ref('')
 const checkedItems = ref([])
-const showItems = computed(() => {
-  const splitItem = checkedItems.value?.map((i) => i?.split('"')?.[1])
-  const a = splitItem.map((i) => Number(i))
+
+// computed
+const showcalculateAmount = computed(() => {
+  const splitItems = checkedItems.value?.map((i) => i?.split('"')?.[1])
+  const convertNumbers = splitItems.map((i) => Number(i))
   const init = 0
-  const calculateAmount = a.reduce((prev, current) => prev + current, init)
+  const calculateAmount = convertNumbers.reduce((prev, current) => prev + current, init)
   return `支払い総額　${Math.abs(calculateAmount)}円`
 })
 
+// methods
 function loadCsvFile(e) {
-  // moneyForwardCsv.value = []
-  // message.value = ""
   const file = e.target.files[0]
 
   if (!file.type.match('text/csv')) {
@@ -52,15 +64,11 @@ function loadCsvFile(e) {
   <div>
     <input type="file" @change="loadCsvFile" />
     <p>{{ message }}</p>
-
     <div>
-      {{ showItems }}
+      {{ showcalculateAmount }}
     </div>
-    <!-- {{ checkedItems.map((i) => Numberi.split('"')[1]) }} -->
-
-    <!-- // .reduce((previousValue, currentValue) => previousValue + currentValue, initialValue) -->
-
-    <table border="1">
+    <table border="2" v-if="moneyForwardCsv.length">
+      <th v-for="c in Constants">{{ c }}</th>
       <tr v-for="(worker, index) in moneyForwardCsv" :key="index">
         <td v-if="worker.usedAmount">
           <input type="checkbox" :id="index" :value="worker.usedAmount" v-model="checkedItems" />
@@ -68,27 +76,7 @@ function loadCsvFile(e) {
         <td v-for="(column, index) in worker" :key="index">{{ column }}</td>
       </tr>
     </table>
-
-    <!-- <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
   </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
